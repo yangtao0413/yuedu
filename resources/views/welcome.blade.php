abc3315a59352b394ec61f34a296f82d043f300c
yangtao <1624530554@qq.com>
2020-03-25 12:40:12 +0800

    YT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    YT
                 </div>
 
                 <div class="links">
